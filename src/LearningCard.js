3d89877f0311c736fac052ddfd5cc7fbc6f4aadf
ejm5763 <ejm5763@gmail.com>
2021-10-24 15:02:39 -0400

    trying to push for the millionth time

--- src/LearningCard.js
+++ src/LearningCard.js
@@ -1,5 +1,6 @@
 // dependencies / things imported
 import { LitElement, html, css } from 'lit';
+import './LearningBanner.js';
 
 // this is the base path to the assets calculated at run time
 // this ensures that assets are shipped correctly when building the demo
@@ -9,6 +10,7 @@ import { LitElement, html, css } from 'lit';
 const beaker = new URL('../assets/beaker.svg', import.meta.url).href;
 const lightbulb = new URL('../assets/lightbulb.svg', import.meta.url).href;
 const question = new URL('../assets/question.svg', import.meta.url).href;
+
 // EXPORT (so make available to other documents that reference this file) a class, that extends LitElement
 // which has the magic life-cycles and developer experience below added
 export class LearningCard extends LitElement {
@@ -16,12 +18,14 @@ export class LearningCard extends LitElement {
   static get tag() {
     return "learning-card";
   }
+
   // HTMLElement life-cycle, built in; use this for setting defaults
   constructor() {
     super();
     this.myIcon = null;
     this.type = 'math';
   }
+
   // properties that you wish to use as data in HTML, CSS, and the updated life-cycle
   static get properties() {
     return {
@@ -32,6 +36,7 @@ export class LearningCard extends LitElement {
       myIcon: { type: String, attribute: "my-icon" },
     };
   }
+
   // updated fires every time a property defined above changes
   // this allows you to react to variables changing and use javascript to perform logic
   updated(changedProperties) {
@@ -41,6 +46,7 @@ export class LearningCard extends LitElement {
       }
     });
   }
+
   // Lit life-cycle; this fires the 1st time the element is rendered on the screen
   // this is a sign it is safe to make calls to this.shadowRoot
   firstUpdated(changedProperties) {
@@ -48,16 +54,19 @@ export class LearningCard extends LitElement {
       super.firstUpdated(changedProperties);
     }
   }
+
   // HTMLElement life-cycle, element has been connected to the page / added or moved
   // this fires EVERY time the element is moved
   connectedCallback() {
     super.connectedCallback();
   }
+
   // HTMLElement life-cycle, element has been removed from the page OR moved
   // this fires every time the element moves
   disconnectedCallback() {
     super.disconnectedCallback();
   }
+
   // CSS - specific to Lit
   static get styles() {
     return css`
@@ -76,9 +85,11 @@ export class LearningCard extends LitElement {
       }
     `;
   }
+
   // HTML - specific to Lit
   render() {
     return html`
+    <pjc-banner> </pjc-banner>
     <h1>cool</h1>
     <div>${this.type}</div>
     <div>
@@ -95,6 +106,7 @@ export class LearningCard extends LitElement {
     </div>
     `;
   }
+  
   // HAX specific callback
   // This teaches HAX how to edit and work with your web component
   /**
